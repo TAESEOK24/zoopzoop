d4d8db335236a8c6bf1e7a2c0de013e910e4e7fb
TAESEOK24 <12345678+dukku@users.noreply.github.com>
2026-03-22 19:13:55 +0900

    init project

---
+++ src/main/java/com/zoopzoop/zoopzoop/ZoopzoopApplication.java
@@ -0,0 +1,13 @@
+package com.zoopzoop.zoopzoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoopzoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoopzoopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zoopzoop/zoopzoop/ZoopzoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoopzoop.zoopzoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoopzoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
