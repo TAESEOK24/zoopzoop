c86ba86dd1920c407ea4451f791b8abdface1b07
TAESEOK24 <rolts1231@naver.com>
2026-04-11 23:19:46 +0900

    fix: CI/CD터짐 현상 수정

--- Back/src/test/java/com/zoopzoop/zoopzoop/ZoopzoopApplicationTests.java
+++ Back/src/test/java/com/zoopzoop/zoopzoop/ZoopzoopApplicationTests.java
@@ -2,8 +2,10 @@ package com.zoopzoop.zoopzoop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestChatbotConfig.class)
 class ZoopzoopApplicationTests {
 
     @Test
