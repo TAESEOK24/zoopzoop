3542c6511f4700c2501ac6557b4cea2ae1d8c829
TAESEOK24 <rolts1231@naver.com>
2026-04-12 00:33:02 +0900

    fix: passwordencoder 중복 삭제

--- Back/src/test/java/com/zoopzoop/zoopzoop/TestSecurityConfig.java
+++ Back/src/test/java/com/zoopzoop/zoopzoop/TestSecurityConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @TestConfiguration
@@ -20,10 +18,5 @@ public class TestSecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
