1097267d41d33483f45881b06d80dad59c7e0776
TAESEOK24 <rolts1231@naver.com>
2026-04-11 23:28:45 +0900

    fix: CI/CD 수정

--- Back/src/main/java/com/zoopzoop/zoopzoop/global/config/SecurityConfig.java
+++ Back/src/main/java/com/zoopzoop/zoopzoop/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.zoopzoop.zoopzoop.global.security.SecurityExceptionHandler;
 import com.zoopzoop.zoopzoop.global.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@Profile("!test")
 public class SecurityConfig {
 
     @Bean
